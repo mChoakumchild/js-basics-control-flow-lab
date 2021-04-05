81cb915628858675fb68c79b2028157e7f8eef8a
Elijah Schiltz-Rouse <elijahforest101@gmail.com>
2021-04-05 11:42:58 -0400

    commit tree 7c7be5e928d7ba2324378c57d60853961ffd8055

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(a){
+  let b;
+  if (a<400) {
+    b = 'This one is on me!';
+
+  } else if (a>2000 && a<2500) {
+    b = 'I will gladly take your thirty bucks.';
+    
+  } else if (a > 2500) {
+    b = 'No can do.';
+  }
+  return b;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(a){
+const b = 'Ok, sounds good.';
+const c = 'No go.';
+return ((a == 'NYC') ? b: c);
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(a){
+  
+  switch(a){
+    case 'generous':
+      return'Thank you so much.';
+      break;
+    case 'not as generous':
+      return'Thank you.';
+      break;
+    default:
+      return'Bye.';
+      break;
+
+  }
+  
 }
